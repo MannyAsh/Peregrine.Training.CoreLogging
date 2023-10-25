f5703d11c83dc0a6c73b42f0b0bc763325306bc7
MannyAsh <amaan.ashiq.aa@gmail.com>
2023-10-25 13:30:25 +0100

    Add project files.

---
+++ CoreLogging/Program.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+
+namespace CoreLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(@"C:\Users\amaanashiq\Documents\csharp\logging\LogFile.txt")
+                //.WriteTo.File(@"LogFile.txt")
+                .CreateLogger();
+                for (int x = 0; x < 2; x++)
+                {
+                    Log.Information($"Hello, Serilog! {x}");
+                    Log.Warning($"Goodbye, Serilog.  {x}");
+                    Log.Information($"Time to add two numbers: {DateTime.Now.ToUniversalTime()}    {x}");
+                    Log.Verbose($"Verbose log message: {x}");
+
+                }
+                Log.CloseAndFlush();
+                Console.WriteLine("---------------------");
+                ReadFromFile();
+                Console.ReadLine();
+            }
+        }
+
+        private static void ReadFromFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\amaanashiq\Documents\csharp\logging\LogFile.txt");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+        }
+    }
+}
+
